ba52f7c0b2ddcac9a363aa22ae4fd9dc588a657e
James Tiplady <biggleszx@biggleszx.com>
2013-10-24 17:06:28 +0100

    Tweaking install script

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ git pull origin master
 ln -s .gitconfig ~/.gitconfig
 
 # append source line to existing .bashrc unless already present
-if [ -f ~/.bashrc && grep -Fxq "# dotfiles bashrc goodies" ~/.bashrc ]
+if [ -f ~/.bashrc ] && grep -Fxq "# dotfiles bashrc goodies" ~/.bashrc
 then
     # found
     echo ".bashrc is already hooked up"
@@ -14,5 +14,5 @@ else
     # not found
     echo "hooking up .bashrc..."
     echo "# dotfiles bashrc goodies" >> ~/.bashrc
-    echo "source $(dirname '${BASH_SOURCE}')/.bashrc" >> ~/.bashrc
+    echo "source `pwd`/.bashrc" >> ~/.bashrc
 fi
