673b7b50a464327f4d4b2778e110365709392aa7
James Tiplady <biggleszx@biggleszx.com>
2013-10-24 17:02:06 +0100

    Finishing off basic install script

--- install.sh
+++ install.sh
@@ -2,6 +2,17 @@
 cd "$(dirname "${BASH_SOURCE}")"
 git pull origin master
 
-# TODO: symlink files to home dir, except .bashrc
+# symlinks
+ln -s .gitconfig ~/.gitconfig
 
-# TODO: append source line for .bashrc to actual ~/.bashrc
+# append source line to existing .bashrc unless already present
+if [ -f ~/.bashrc && grep -Fxq "# dotfiles bashrc goodies" ~/.bashrc ]
+then
+    # found
+    echo ".bashrc is already hooked up"
+else
+    # not found
+    echo "hooking up .bashrc..."
+    echo "# dotfiles bashrc goodies" >> ~/.bashrc
+    echo "source $(dirname '${BASH_SOURCE}')/.bashrc" >> ~/.bashrc
+fi
