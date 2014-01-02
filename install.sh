c225ff044926ded1a87895ac1a45d9d2adce2de8
James Tiplady <biggleszx@biggleszx.com>
2014-01-02 11:35:35 +0000

    Fixing gitconfig simlink creation; adding git user/email prompt

--- install.sh
+++ install.sh
@@ -2,9 +2,6 @@
 cd "$(dirname "${BASH_SOURCE}")"
 git pull origin master
 
-# symlinks
-ln -s .gitconfig ~/.gitconfig
-
 # append source line to existing .bashrc unless already present
 if [ -f ~/.bashrc ] && grep -Fxq "# dotfiles bashrc goodies" ~/.bashrc
 then
@@ -16,3 +13,15 @@ else
     echo "# dotfiles bashrc goodies" >> ~/.bashrc
     echo "source `pwd`/.bashrc" >> ~/.bashrc
 fi
+
+# symlinks
+cd ~
+ln -s dotfiles/.gitconfig .gitconfig
+
+# prompt for git user.name and user.email
+read -p "Please enter your name for git config:" gitname
+git config --global user.name "$gitname"
+read -p "Please enter your email for git config:" gitemail
+git config --global user.email $gitemail
+
+echo "Dotfiles setup complete!"
